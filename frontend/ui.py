30814815aaf31dd61c6361ce3af4432fa08ae758
Sergey Zinov <sergey.zinov@gmail.com>
2016-01-03 16:19:25 +0100

    added event handlers for buttons

--- frontend/ui.py
+++ frontend/ui.py
@@ -21,10 +21,12 @@ class mainWindow(object):
         
         frame = gtk.Frame("")
         label = gtk.Label("Game state here")
+        self.status_label = label
         frame.add(label)        
         hbox.pack_start(frame)
         
         button = gtk.Button("Restart")
+        button.connect("clicked", self.restart_button_click)
         hbox.pack_start(button)
         
         board = self.make_board()
@@ -34,12 +36,21 @@ class mainWindow(object):
     
     def make_board(self):
         table = gtk.Table(rows=3, columns=3)
+        self.buttons = [[0 for i in xrange(3)] for i in xrange(3)]
         for row in xrange(3):
             for column in xrange(3):
                 button = gtk.Button("X")
+                self.buttons[row][column] = button 
+                button.connect("clicked", self.board_button_click, (row, column))
                 table.attach(button, row, row+1, column, column+1)
         return table
 
+    def board_button_click(self, button, field):
+        pass
+    
+    def restart_button_click(self, button):
+        pass
+    
 def main():
     mainWindow()
     gtk.main()
