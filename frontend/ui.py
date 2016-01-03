9150175e905e88e268c1f5a9d0914a115d8db425
Sergey Zinov <sergey.zinov@gmail.com>
2016-01-03 18:32:47 +0100

    move mainWindow in separate file

--- frontend/ui.py
+++ frontend/ui.py
@@ -5,9 +5,9 @@ Created on 03.01.2016
 @author: sergey
 '''
 
-import pygtk
 import gtk
 import backend.Game
+from mainWindow import mainWindow
 
 class tictactoe_ui(object):
     def __init__(self, game):
@@ -17,6 +17,8 @@ class tictactoe_ui(object):
         
         self.mainWindow.board_button_click = self.board_button_click
         self.mainWindow.restart_button_click = self.restart_button_click
+        
+        self.update_board()
 
     def update_board(self):
         for row in xrange(3):
@@ -41,62 +43,6 @@ class tictactoe_ui(object):
     def run(self):
         gtk.main()
 
-class mainWindow(object):
-    def __init__(self):
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.connect("destroy", gtk.main_quit)
-        self.window.set_title("Tic-Tac-Toe")
-        
-        vbox = gtk.VBox(spacing = 4)
-        self.window.add(vbox)
-        
-        hbox = gtk.HBox(spacing = 4)
-        vbox.pack_start(hbox)
-        
-        frame = gtk.Frame("")
-        label = gtk.Label("Game state here")
-        self.status_label = label
-        frame.add(label)        
-        hbox.pack_start(frame)
-        
-        button = gtk.Button("Restart")
-        button.connect("clicked", self._restart_button_click)
-        hbox.pack_start(button)
-        
-        board = self.make_board()
-        vbox.pack_start(board)
-        
-        self.window.show_all()
-    
-    def make_board(self):
-        table = gtk.Table(rows=3, columns=3)
-        self.buttons = [[0 for i in xrange(3)] for i in xrange(3)]
-        for row in xrange(3):
-            for column in xrange(3):
-                button = gtk.Button("X")
-                self.buttons[row][column] = button 
-                button.connect("clicked", self._board_button_click, (row, column))
-                table.attach(button, row, row+1, column, column+1)
-        return table
-    
-    def _board_button_click(self, button, field):
-        self.board_button_click(button, field)
-        
-    def board_button_click(self, button, field):
-        raise Exception("Should be overridden")
-    
-    def _restart_button_click(self, button):
-        self.restart_button_click(button)
-        
-    def restart_button_click(self, button):
-        raise Exception("Should be overridden")
-    
-def main():
-    mainWindow()
-    gtk.main()
-    return 0
 
-if __name__ == "__main__":
-    main()
 
 
